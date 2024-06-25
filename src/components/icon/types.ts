53114df5d951b393fd1e42fb291bb0ac4f994fdd
Akindeju Oluwagbemiga Michael <akindejuhmikel@gmail.com>
2024-06-25 09:50:15 +0100

    fix: lint

--- src/components/icon/types.ts
+++ src/components/icon/types.ts
@@ -1,20 +1,20 @@
 import { StyleProp, ViewStyle } from 'react-native';
 import { SvgProps } from 'react-native-svg';
 
-import backArrow from '../../assets/svg/back-arrow.svg'
-import googleLogo from '../../assets/svg/google-logo.svg'
-import home from '../../assets/svg/home.svg'
-import pwdOff from '../../assets/svg/pwd-off.svg'
-import pwdOn from '../../assets/svg/pwd-on.svg'
-import share from '../../assets/svg/share.svg'
+import backArrow from '../../assets/svg/back-arrow.svg';
+import googleLogo from '../../assets/svg/google-logo.svg';
+import home from '../../assets/svg/home.svg';
+import pwdOff from '../../assets/svg/pwd-off.svg';
+import pwdOn from '../../assets/svg/pwd-on.svg';
+import share from '../../assets/svg/share.svg';
 
 export const ICONS = {
-  "back-arrow": backArrow,
-  "google-logo": googleLogo,
-  "home": home,
-  "pwd-off": pwdOff,
-  "pwd-on": pwdOn,
-  "share": share
+  'back-arrow': backArrow,
+  'google-logo': googleLogo,
+  home: home,
+  'pwd-off': pwdOff,
+  'pwd-on': pwdOn,
+  share: share,
 };
 
 export type IconName = keyof typeof ICONS;
